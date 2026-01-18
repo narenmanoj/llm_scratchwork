189003d64468c84bc8a8bb36334e125f2be0cc0e
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-18 13:10:36 -0500

    flash attention triton boilerplate

--- triton_attention.py
+++ triton_attention.py
@@ -6,91 +6,6 @@ import triton.language as tl
 
 from custom_modules import scaled_dot_product_attention
 
-@triton.jit
-def flash_fwd_kernel(
-    Q_ptr, K_ptr, V_ptr, O_ptr, L_ptr,
-    stride_qb, stride_qq, stride_qd,
-    stride_kb, stride_kk, stride_kd,
-    stride_vb, stride_vk, stride_vd,
-    stride_ob, stride_oq, stride_od,
-    stride_lb, stride_lq,
-    N_QUERIES, N_KEYS,
-    scale,
-    D: tl.constexpr,
-    Q_TILE_SIZE: tl.constexpr,
-    K_TILE_SIZE: tl.constexpr,
-):
-    # Program indices
-    query_tile_index = tl.program_id(0)
-    batch_index = tl.program_id(1)
-
-    # Offset each pointer with the corresponding batch index
-    # multiplied with the batch stride for each tensor
-    Q_block_ptr = tl.make_block_ptr(
-        Q_ptr + batch_index * stride_qb,
-        shape=(N_QUERIES, D),
-        strides=(stride_qq, stride_qd),
-        offsets=(query_tile_index * Q_TILE_SIZE, 0),
-        block_shape=(Q_TILE_SIZE, D),
-        order=(1, 0),
-    )
-
-    N_TILES_KV = math.ceil(N_KEYS / K_TILE_SIZE)
-
-    # K_block_ptr = tl.make_block_ptr(
-    #     K_ptr + batch_index * stride_kb,
-    #     shape=(N_KEYS, D),
-    #     strides=(stride_kk, stride_kd),
-    #     offsets=(key_tile_index * K_TILE_SIZE, 0),
-    #     block_shape=(K_TILE_SIZE, D),
-    #     order=(1, 0),
-    # )
-
-    # V_block_ptr = tl.make_block_ptr(
-    #     V_ptr + batch_index * stride_vb,
-    #     shape=(N_KEYS, D),
-    #     strides=(stride_vk, stride_vd),
-    #     offsets=(key_tile_index * K_TILE_SIZE, 0),
-    #     block_shape=(K_TILE_SIZE, D),
-    #     order=(1, 0),
-    # )
-
-    output = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
-
-    raise NotImplementedError
-
-class TritonAttention(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, query, key, value, attn_mask=None):
-        ctx.Q_TILE_SIZE = 16
-        ctx.K_TILE_SIZE = 16
-        D = query.shape[-1]
-        scale = math.sqrt(D)
-        N_BATCHES = query.shape[:-2]
-        N_QUERIES = query.shape[-2]
-        N_KEYS = key.shape[-2]
-
-        ctx.N_TILES_Q = math.ceil(N_QUERIES / ctx.Q_TILE_SIZE)
-        
-        O = torch.empty_like(query)
-        L = torch.empty(query.shape[:-1])
-        flash_fwd_kernel[(ctx.N_TILES_Q, N_BATCHES)](
-            query, key, value, O, L,
-            query.stride(0), query.stride(1), query.stride(2),
-            key.stride(0), key.stride(1), key.stride(2),
-            value.stride(0), value.stride(1), value.stride(2),
-            O.stride(0), O.stride(1), O.stride(2),
-            L.stride(0), L.stride(1),
-            N_QUERIES, N_KEYS,
-            scale, D, ctx.Q_TILE_SIZE, ctx.K_TILE_SIZE
-        )
-        to_save = [L, query, key, value, O]
-        ctx.save_for_backward(*to_save)
-        return O
-    
-    @staticmethod
-    def backward(ctx, grad_out):
-        raise NotImplementedError
 
 class PyTorchFlashAttention(torch.autograd.Function):
     @staticmethod
@@ -143,6 +58,97 @@ class PyTorchFlashAttention(torch.autograd.Function):
         ctx.save_for_backward(*to_save)
         return O
 
+    @staticmethod
+    def backward(ctx, grad_out):
+        raise NotImplementedError
+
+
+@triton.jit
+def flash_fwd_kernel(
+    Q_ptr, K_ptr, V_ptr, O_ptr, L_ptr,
+    stride_qb, stride_qq, stride_qd,
+    stride_kb, stride_kk, stride_kd,
+    stride_vb, stride_vk, stride_vd,
+    stride_ob, stride_oq, stride_od,
+    stride_lb, stride_lq,
+    N_QUERIES, N_KEYS,
+    scale,
+    D: tl.constexpr,
+    Q_TILE_SIZE: tl.constexpr,
+    K_TILE_SIZE: tl.constexpr,
+    is_causal: tl.constexpr,
+):
+    # Program indices
+    query_tile_index = tl.program_id(0)
+    batch_index = tl.program_id(1)
+
+    # Offset each pointer with the corresponding batch index
+    # multiplied with the batch stride for each tensor
+    Q_block_ptr = tl.make_block_ptr(
+        Q_ptr + batch_index * stride_qb,
+        shape=(N_QUERIES, D),
+        strides=(stride_qq, stride_qd),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1, 0),
+    )
+
+    N_TILES_KV = math.ceil(N_KEYS / K_TILE_SIZE)
+
+    # K_block_ptr = tl.make_block_ptr(
+    #     K_ptr + batch_index * stride_kb,
+    #     shape=(N_KEYS, D),
+    #     strides=(stride_kk, stride_kd),
+    #     offsets=(key_tile_index * K_TILE_SIZE, 0),
+    #     block_shape=(K_TILE_SIZE, D),
+    #     order=(1, 0),
+    # )
+
+    # V_block_ptr = tl.make_block_ptr(
+    #     V_ptr + batch_index * stride_vb,
+    #     shape=(N_KEYS, D),
+    #     strides=(stride_vk, stride_vd),
+    #     offsets=(key_tile_index * K_TILE_SIZE, 0),
+    #     block_shape=(K_TILE_SIZE, D),
+    #     order=(1, 0),
+    # )
+
+    output = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
+    for i in range(N_TILES_KV):
+        pass
+
+    raise NotImplementedError
+
+class TritonAttention(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, query, key, value, is_causal=False):
+        ctx.Q_TILE_SIZE = 16
+        ctx.K_TILE_SIZE = 16
+        D = query.shape[-1]
+        scale = math.sqrt(D)
+        N_BATCHES = query.shape[:-2]
+        N_QUERIES = query.shape[-2]
+        N_KEYS = key.shape[-2]
+
+        ctx.N_TILES_Q = math.ceil(N_QUERIES / ctx.Q_TILE_SIZE)
+        
+        O = torch.empty_like(query)
+        L = torch.empty(query.shape[:-1])
+        flash_fwd_kernel[(ctx.N_TILES_Q, N_BATCHES)](
+            query, key, value, O, L,
+            query.stride(0), query.stride(1), query.stride(2),
+            key.stride(0), key.stride(1), key.stride(2),
+            value.stride(0), value.stride(1), value.stride(2),
+            O.stride(0), O.stride(1), O.stride(2),
+            L.stride(0), L.stride(1),
+            N_QUERIES, N_KEYS,
+            scale, D, ctx.Q_TILE_SIZE, ctx.K_TILE_SIZE,
+            is_causal
+        )
+        to_save = [L, query, key, value, O]
+        ctx.save_for_backward(*to_save)
+        return O
+    
     @staticmethod
     def backward(ctx, grad_out):
         raise NotImplementedError
